36297e2dc64ee0080ab6b6b104d497dcd706df16
sowjanya.gollapathi <sowjanya.gollapathi@gmail.com>
2022-01-11 16:58:37 +0530

    Add project files.

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = null;
+            try
+            {
+                con = new SqlConnection(@"data source = (localdb)\MSSQLLocalDB;database=NorthWind;integrated security = SSPI");
+                SqlCommand cm = new SqlCommand("select * from Customers", con);
+                con.Open();
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["CompanyName"] + " " + sdr["Region"]);
+                }
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something is Wrong" + e);
+            }
+            
+        }
+    }
+}
